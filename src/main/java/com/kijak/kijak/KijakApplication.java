defb235cac6993c58ec505cf2cfd10a2761632dd
onehappypie <61019405+onehappypie@users.noreply.github.com>
2020-11-29 00:12:47 +0100

    Initial commit

---
+++ src/main/java/com/kijak/kijak/KijakApplication.java
@@ -0,0 +1,13 @@
+package com.kijak.kijak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KijakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KijakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kijak/kijak/KijakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kijak.kijak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KijakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
